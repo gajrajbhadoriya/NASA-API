6ed0d3fb8e649781e736571d47c7c4b3e4b5c432
gajrajbhadoriya <125948781+gajrajbhadoriya@users.noreply.github.com>
2023-03-10 19:10:35 +0530

    Add files via upload

---
+++ helpers/helper.php
@@ -0,0 +1,24 @@
+<?php
+
+function getMinMaxVal(array $array, string $col, string $type= 'max')
+{
+    // dd($array);
+    $val = $array[array_key_first($array)];
+
+    foreach ($array as $arr) {
+        $valIsMinMax = ($type === 'max' && (abs($val[$col]) < abs($arr[$col]))) || ($type === 'min' && (abs($val[$col]) > abs($arr[$col])));
+        if ($valIsMinMax){
+            $val = $arr;
+            // continue;
+        }
+
+        // 123
+        //123
+    }
+    return $val;
+}
+
+function getPostData()
+{
+    return $_POST;
+}
